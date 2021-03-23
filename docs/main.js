c29ec9aafa57566d716d1f1c28c61ca3e865816e
Musab Hassan <musabhassan04@gmail.com>
2021-03-23 13:53:08 -0700

    Bug Fixes for showcase webpage

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,17 @@
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => { 
     slickScroll.momentumScroll({
         root: "body",
         duration: 1000,
         easing: "cubic-bezier(0.25, 1, 0.5, 1)",
         offsets: [{
             element: ".slow-parallax",
-            speedY: 0.86
+            speedY: 0.8
         }, {
             element: ".faster-parallax",
-            speedY: 0.8
-        }]
+            speedY: 0.7
+        }],
+        fixed: [
+            ".nav-container"
+        ]
     });
-}
+}, false);
