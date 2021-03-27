4a27684a1393992619c5d4f2b79eb97a01b924cc
Musab Hassan <musabhassan04@gmail.com>
2021-03-27 16:52:57 -0700

    Make penner functions parsable

--- src/index.es.js
+++ src/index.es.js
@@ -9,19 +9,50 @@
 ***************************************/
 
 
-export class slickScroll {
+export default class slickScroll {
 
     // Defaults
-    offsets = [];
-    fixed = [];
     defaults = {
         root: "body",
         duration: 1000,
         easing: "cubic-bezier(0.15, 1, 0.4, 1)",
         offsets: {speedX: 1, speedY: 1}
-    }
+    };
+    presetEasings = [
+        // Sine
+        {name: "easeInSine", value: [0.12, 0, 0.39, 0]},
+        {name: "easeOutSine", value: [0.61, 1, 0.88, 1]},
+        {name: "easeInOutSine", value: [0.37, 0, 0.63, 1]},
+        // Quad
+        {name: "easeInQuad", value: [0.11, 0, 0.5, 0]},
+        {name: "easeOutQuad", value: [0.5, 1, 0.89, 1]},
+        {name: "easeInOutQuad", value: [0.45, 0, 0.55, 1]},
+        // Cubic
+        {name: "easeInCubic", value: [0.32, 0, 0.67, 0]},
+        {name: "easeOutCubic", value: [0.33, 1, 0.68, 1]},
+        {name: "easeInOutCubic", value: [0.65, 0, 0.35, 1]},
+        // Quart
+        {name: "easeInQuart", value: [0.5, 0, 0.75, 0]},
+        {name: "easeOutQuart", value: [0.25, 1, 0.5, 1]},
+        {name: "easeInOutQuart", value: [0.76, 0, 0.24, 1]},
+        // Quint
+        {name: "easeInQuint", value: [0.64, 0, 0.78, 0]},
+        {name: "easeOutQuint", value: [0.22, 1, 0.36, 1]},
+        {name: "easeInOutQuint", value: [0.83, 0, 0.17, 1]},
+        // Expo
+        {name: "easeInExpo", value: [0.7, 0, 0.84, 0]},
+        {name: "easeOutExpo", value: [0.16, 1, 0.3, 1]},
+        {name: "easeInOutExpo", value: [0.87, 0, 0.13, 1]},
+        // Circ
+        {name: "easeInCirc", value: [0.55, 0, 1, 0.45]},
+        {name: "easeOutCirc", value: [0, 0.55, 0.45, 1]},
+        {name: "easeInOutCirc", value: [0.85, 0, 0.15, 1]}
+    ];
 
-    
+
+    offsets = [];
+    fixed = [];
+
     momentumScroll(dataObj) {
         // Set unset properties to defaults
         dataObj = Object.assign({}, this.defaults, dataObj);
@@ -116,7 +147,6 @@ export class slickScroll {
                         dx = (diffX * bezier.apply(null, easing)(t)) + tl.x;
                         dy = (diffY * bezier.apply(null, easing)(t)) + tl.y;
 
-                        // Fix JS Rounding
                         dx = Math.ceil(dx * 100) / 100;
                         dy = Math.ceil(dy * 100) / 100;
                         onIterate({x: dx, y: dy});
@@ -126,9 +156,19 @@ export class slickScroll {
                 }());
 
                 function parseBezier(bezierString) {
-                    bezierString = bezierString.split(/([^\(-\)]*)/);
-                    bezierString = bezierString[3].split(/,(?![^()]*\()/);
-                    return bezierString.map((x) => { return parseFloat(x) });
+                    let vals = classThis.presetEasings.filter(e => e.name == bezierString);
+                    if (vals[0]) {
+                        vals = vals[0].value;
+                    } else {
+                        bezierString = bezierString.split(/([^\(-\)]*)/);
+                        bezierString = bezierString[3].split(/,(?![^()]*\()/);
+                        vals = bezierString.map((x) => { return parseFloat(x) });
+                    }
+
+                    if (Array.isArray(vals) && vals.length == 4)
+                        return vals;
+                    else
+                        throw "easing string is invalid.";
                 }
             }
         }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ var slickScroll;
 (function() {
 
 
-    // Defaults in case of empty properties
+    // Defaults
     var defaults = {
         root: "body",
         duration: 1000,
@@ -24,6 +24,37 @@ var slickScroll;
             speedY: 1
         }
     }
+
+    var presetEasings = [
+        // Sine
+        {name: "easeInSine", value: [0.12, 0, 0.39, 0]},
+        {name: "easeOutSine", value: [0.61, 1, 0.88, 1]},
+        {name: "easeInOutSine", value: [0.37, 0, 0.63, 1]},
+        // Quad
+        {name: "easeInQuad", value: [0.11, 0, 0.5, 0]},
+        {name: "easeOutQuad", value: [0.5, 1, 0.89, 1]},
+        {name: "easeInOutQuad", value: [0.45, 0, 0.55, 1]},
+        // Cubic
+        {name: "easeInCubic", value: [0.32, 0, 0.67, 0]},
+        {name: "easeOutCubic", value: [0.33, 1, 0.68, 1]},
+        {name: "easeInOutCubic", value: [0.65, 0, 0.35, 1]},
+        // Quart
+        {name: "easeInQuart", value: [0.5, 0, 0.75, 0]},
+        {name: "easeOutQuart", value: [0.25, 1, 0.5, 1]},
+        {name: "easeInOutQuart", value: [0.76, 0, 0.24, 1]},
+        // Quint
+        {name: "easeInQuint", value: [0.64, 0, 0.78, 0]},
+        {name: "easeOutQuint", value: [0.22, 1, 0.36, 1]},
+        {name: "easeInOutQuint", value: [0.83, 0, 0.17, 1]},
+        // Expo
+        {name: "easeInExpo", value: [0.7, 0, 0.84, 0]},
+        {name: "easeOutExpo", value: [0.16, 1, 0.3, 1]},
+        {name: "easeInOutExpo", value: [0.87, 0, 0.13, 1]},
+        // Circ
+        {name: "easeInCirc", value: [0.55, 0, 1, 0.45]},
+        {name: "easeOutCirc", value: [0, 0.55, 0.45, 1]},
+        {name: "easeInOutCirc", value: [0.85, 0, 0.15, 1]}
+    ];
 
     var offsets = {};
     var fixed = {};
@@ -135,9 +166,19 @@ var slickScroll;
 
                     // Bezier String Parser
                     function parseBezier(bezierString) {
-                        bezierString = bezierString.split(/([^\(-\)]*)/);
-                        bezierString = bezierString[3].split(/,(?![^()]*\()/);
-                        return bezierString.map((x) => { return parseFloat(x) });
+                        let vals = presetEasings.filter(e => e.name == bezierString);
+                        if (vals[0]) {
+                            vals = vals[0].value;
+                        } else {
+                            bezierString = bezierString.split(/([^\(-\)]*)/);
+                            bezierString = bezierString[3].split(/,(?![^()]*\()/);
+                            vals = bezierString.map((x) => { return parseFloat(x) });
+                        }
+                        
+                        if (Array.isArray(vals) && vals.length == 4)
+                            return vals;
+                        else
+                            throw "easing string is invalid.";
                     }
                 }
             }
