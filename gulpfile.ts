e6d4aa9e51ae45f1249c2a8a6c28073058c1b1ef
Musab Hassan <musabhassan04@gmail.com>
2021-12-22 14:53:28 -0800

    Seperate production builds from dev builds

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,23 @@
+/**************************************
+*
+*  Build production builds with 'npm run build'
+*  Watch to build for production with 'npm run watch'
+*  Build dev builds with 'npm run dev'
+*  Watch to build dev builds with 'npm run watch-dev'
+*
+***************************************/
+
+
 const { src, dest, task, parallel, watch } = require('gulp');
 const webpack = require("webpack-stream");
 
 const webpackConfig = require('./webpack.config.js');
 
+/* Production Builds */
 const buildES6 = function() {
 
     let config = {...webpackConfig, ...{
+        mode: "production",
         experiments: {
             outputModule: true,
         },
@@ -25,10 +37,11 @@ const buildES6 = function() {
 const buildES5 = function() {
     let config = {
         ...webpackConfig, ...{
+            mode: "production",
             output: {
                 filename: 'slickscroll.min.js',
                 library: {
-                    name: "slickscroll",
+                    name: "slickScroll",
                     type: 'var'
                 }
             }
@@ -43,10 +56,11 @@ const buildES5 = function() {
 const buildNode = function() {
     let config = {
         ...webpackConfig, ...{
+            mode: "production",
             output: {
                 filename: 'slickscroll.node.min.js',
                 library: {
-                    name: "slickscroll",
+                    name: "slickScroll",
                     type: 'commonjs'
                 }
             }
@@ -58,7 +72,85 @@ const buildNode = function() {
         .pipe(dest("dist/"))
 }
 
-task("build", parallel(buildES5, buildES6, buildNode))
+task("build", parallel(buildES5, buildES6, buildNode));
 task('watch', function () {
-    watch('src/index.ts', parallel(buildES5, buildES6, buildNode));
-})
+    watch('src/**', parallel(buildES5, buildES6, buildNode));
+});
+
+
+
+
+
+
+
+
+
+
+/* Dev Builds */
+const buildES6Dev = function () {
+
+    let config = {
+        ...webpackConfig, ...{
+            mode: "development",
+            devtool: "source-map",
+            experiments: {
+                outputModule: true,
+            },
+            output: {
+                filename: 'build.es.js',
+                library: {
+                    type: 'module'
+                }
+            }
+        }
+    };
+
+    return src("src/index.ts")
+        .pipe(webpack(config))
+        .pipe(dest("builds/"))
+}
+
+const buildES5Dev = function () {
+    let config = {
+        ...webpackConfig, ...{
+            mode: "development",
+            devtool: "source-map",
+            output: {
+                filename: 'build.js',
+                library: {
+                    name: "slickScroll",
+                    type: 'var'
+                }
+            }
+        }
+    };
+
+    return src("src/index.ts")
+        .pipe(webpack(config))
+        .pipe(dest("builds/"))
+}
+
+const buildNodeDev = function () {
+    let config = {
+        ...webpackConfig, ...{
+            mode: "development",
+            devtool: "source-map",
+            output: {
+                filename: 'build.node.js',
+                library: {
+                    name: "slickScroll",
+                    type: 'commonjs'
+                }
+            }
+        }
+    };
+
+    return src("src/index.ts")
+        .pipe(webpack(config))
+        .pipe(dest("builds/"))
+}
+
+task("dev", parallel(buildES5Dev, buildES6Dev, buildNodeDev));
+task('watch-dev', function () {
+    watch('src/**', parallel(buildES5Dev, buildES6Dev, buildNodeDev));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
   module: {
     rules: [
       {
