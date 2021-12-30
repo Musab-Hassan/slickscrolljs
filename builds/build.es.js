0d8b46f7f7d832d8eaffab789ff840313a171c8f
Musab Hassan <musabhassan04@gmail.com>
2021-12-29 21:33:25 -0800

    Update dev builds

--- builds/build.es.js
+++ builds/build.es.js
@@ -88,8 +88,8 @@ function momentumScroll(dataObj) {
     // Set any offsets or fixedOffsets assigned on initialization
     (0,_utils__WEBPACK_IMPORTED_MODULE_0__.setOffsetArray)(fixedOffsets, rootElem, dataObj.fixedOffsets);
     (0,_utils__WEBPACK_IMPORTED_MODULE_0__.setOffsetArray)(offsets, rootElem, dataObj.offsets);
-    // if client is phone or unsupported
-    if (!isCompatible()) {
+    // if client is a phone or unsupported (supported browsers in README)
+    if (!(0,_utils__WEBPACK_IMPORTED_MODULE_0__.isCompatible)()) {
         let activeFixedOffsets = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getFromOffsetArray)(fixedOffsets, rootElem);
         rootElem.addEventListener("scroll", (event) => {
             let activeOffsets = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getFromOffsetArray)(offsets, rootElem);
@@ -116,23 +116,23 @@ function momentumScroll(dataObj) {
             });
         }
         return {
-            // TODO: Add mobile on Destroy
             destroy: onDestroy,
             removeOffset: removeOffset,
             addOffset: addOffset,
             addFixedOffset: addFixedOffset
         };
     }
-    // if not phone or unsupported then then ->
     let fixedElem = DOMRestructure(rootElem);
-    let mutationObserver = new MutationObserver(onResize);
+    let matrix = new WebKitCSSMatrix(window.getComputedStyle(fixedElem.fixed).transform);
+    let lastLocation = { x: matrix.m41, y: matrix.m42 };
     // Detect any changes to root element's appearance
-    window.addEventListener("resize", onResize);
+    let mutationObserver = new MutationObserver(onResize);
     mutationObserver.observe(rootElem, {
         childList: true,
         attributes: true,
         subtree: true
     });
+    window.addEventListener("resize", onResize);
     // Scroll handler
     rootElem.addEventListener("scroll", onScroll);
     return {
@@ -144,30 +144,24 @@ function momentumScroll(dataObj) {
     /* Event Handler Functions */
     // Scroll handler
     function onScroll(e) {
-        if (dataObj.onScroll)
-            dataObj.onScroll(e); // Run passed onScroll function
         let activeOffsets = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getFromOffsetArray)(offsets, rootElem);
         let activeFixedOffsets = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getFromOffsetArray)(fixedOffsets, rootElem);
         // Get scroll location of rootElement
         pl = { x: rootElem.scrollLeft, y: rootElem.scrollTop };
         if (typeof pl.x === "undefined" || typeof pl.y === "undefined")
             pl = { x: rootElem.scrollX, y: rootElem.scrollY, };
-        // Get transform coordinates of fixedElements
-        let style = window.getComputedStyle(fixedElem.fixed);
-        let matrix = new WebKitCSSMatrix(style.transform);
-        let tl = { x: matrix.m41, y: matrix.m42 };
         // Current time for timing to work
         startStamp = Date.now();
         // Scroll Animation Frame Handler for easing
-        easeFrames(tl, pl, startStamp, (position) => {
+        easeFrames(lastLocation, pl, startStamp, (position) => {
             let translateString = `translate(${position.x}px, ${position.y}px)`;
             fixedElem.fixed.style.webkitTransform = translateString;
             fixedElem.fixed.style.transform = translateString;
+            lastLocation = position;
             // Offset elements scrolling if there are any present
             if (Array.isArray(activeOffsets)) {
                 for (let i = 0; i < activeOffsets.length; i++) {
-                    let e = activeOffsets[i];
-                    e = Object.assign({}, _defaults__WEBPACK_IMPORTED_MODULE_1__.defaultSpeeds, e);
+                    let e = Object.assign({}, _defaults__WEBPACK_IMPORTED_MODULE_1__.defaultSpeeds, activeOffsets[i]);
                     let offset = `translate(${position.x * (e.speedX - 1)}px, ${position.y * (e.speedY - 1)}px)`;
                     let elements = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.selectNode)(e.element, true);
                     if (NodeList.prototype.isPrototypeOf(elements)) {
@@ -185,43 +179,55 @@ function momentumScroll(dataObj) {
             // set fixedOffsets as fixed
             if (Array.isArray(activeFixedOffsets)) {
                 for (let i = 0; i < activeFixedOffsets.length; i++) {
-                    let offset = `translate(${position.x * -1}px, ${position.y * -1}px)`;
-                    let elements = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.selectNode)(activeFixedOffsets[i], true);
-                    if (NodeList.prototype.isPrototypeOf(elements)) {
-                        for (let e of elements) {
+                    let offset = `translate(${-position.x}px, ${-position.y}px)`;
+                    let node = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.selectNode)(activeFixedOffsets[i], true);
+                    if (NodeList.prototype.isPrototypeOf(node)) {
+                        for (let e of node) {
                             e.style.webkitTransform = offset;
                             e.style.transform = offset;
                         }
                     }
                     else {
-                        elements.style.webkitTransform = offset;
-                        elements.style.transform = offset;
+                        node.style.webkitTransform = offset;
+                        node.style.transform = offset;
                     }
                 }
             }
         });
-        // Returns calculated transform values based on scroll position
-        function easeFrames(tl, pl, startStamp, onIterate) {
-            // Parse bezier easing string into number values
-            let easing = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.parseBezierString)(dataObj.easing);
-            let diffX = ((tl.x * -1) - pl.x);
-            let diffY = ((tl.y * -1) - pl.y);
-            let x, y;
-            // Animation frame loop
-            (function loop() {
-                let t = (Date.now() - startStamp) / dataObj.duration;
-                if (t > 1)
-                    t = 1.01;
-                if (t < 1) {
-                    x = (diffX * _utils__WEBPACK_IMPORTED_MODULE_0__.bezierEasing.apply(null, easing)(t)) + tl.x;
-                    y = (diffY * _utils__WEBPACK_IMPORTED_MODULE_0__.bezierEasing.apply(null, easing)(t)) + tl.y;
-                    x = Math.ceil(x * 100) / 100;
-                    y = Math.ceil(y * 100) / 100;
+        if (dataObj.onScroll)
+            asyncScrollFunction(e);
+    }
+    // Run dataObj.onScroll function asynchrounously
+    function asyncScrollFunction(e) {
+        return new Promise(r => {
+            dataObj.onScroll(e);
+            r(true);
+        });
+    }
+    // Returns calculated transform values based on scroll position
+    function easeFrames(tl, pl, startStamp, onIterate) {
+        // Parse bezier easing string into number values
+        let easing = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.parseBezierString)(dataObj.easing);
+        let diffX = ((tl.x * -1) - pl.x);
+        let diffY = ((tl.y * -1) - pl.y);
+        let x, y;
+        // Animation frame loop
+        (function loop() {
+            let t = (Date.now() - startStamp) / dataObj.duration;
+            if (t > 1)
+                t = 1.01;
+            if (t < 1) {
+                x = (diffX * _utils__WEBPACK_IMPORTED_MODULE_0__.bezierEasing.apply(null, easing)(t)) + tl.x;
+                y = (diffY * _utils__WEBPACK_IMPORTED_MODULE_0__.bezierEasing.apply(null, easing)(t)) + tl.y;
+                x = Math.ceil(x * 100) / 100;
+                y = Math.ceil(y * 100) / 100;
+                new Promise(r => {
                     onIterate({ x: x, y: y });
-                    window.requestAnimationFrame(loop);
-                }
-            }());
-        }
+                    r(true);
+                });
+                window.requestAnimationFrame(loop);
+            }
+        }());
     }
     // Remove all slickScroll handers and return DOM to original state
     function onDestroy() {
@@ -347,15 +353,6 @@ function momentumScroll(dataObj) {
             dummy: root.querySelector("div._SS_dummy")
         };
     }
-    // Checks for mobile & unsupported browsers
-    function isCompatible() {
-        let check = false;
-        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-            check = true; })(navigator.userAgent || navigator.vendor);
-        if (!check && CSS.supports)
-            check = !CSS.supports("position", "sticky");
-        return !check;
-    }
 }
 
 
@@ -375,6 +372,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "getFromOffsetArray": () => (/* binding */ getFromOffsetArray),
 /* harmony export */   "selectNode": () => (/* binding */ selectNode),
 /* harmony export */   "parseBezierString": () => (/* binding */ parseBezierString),
+/* harmony export */   "isCompatible": () => (/* binding */ isCompatible),
 /* harmony export */   "bezierEasing": () => (/* binding */ bezierEasing)
 /* harmony export */ });
 /* harmony import */ var _momentumScroll_defaults__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./momentumScroll/defaults */ "./src/momentumScroll/defaults.ts");
@@ -467,6 +465,17 @@ function parseBezierString(bezierString) {
         return vals;
     else
         throw "easing string is invalid.";
+}
+// Checks for mobile & unsupported browsers
+function isCompatible() {
+    let check = false;
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substring(0, 4)))
+            check = true;
+    })(navigator.userAgent || navigator.vendor);
+    if (!check && CSS.supports)
+        check = !CSS.supports("position", "sticky");
+    return !check;
 }
 // ES6 version of gre/bezier-easing 
 // https://github.com/gre/bezier-easing/blob/master/src/index.js
