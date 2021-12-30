851b177dcbc7a22765715b83475c0e7764200ab4
Musab Hassan <musabhassan04@gmail.com>
2021-12-29 22:29:48 -0800

    Remove support for inView

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,8 @@
 
 
 import { momentumScroll } from "./momentumScroll/main";
-import { inView } from "./inView/main"
 
 export default class slickScroll {
     // mometumScrolling
     public momentumScroll = momentumScroll;
-
-    public inView = inView;
 }
