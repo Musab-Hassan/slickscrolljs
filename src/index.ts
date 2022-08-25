693874ca54873dcfdabe8ec901bc6b87573bcd0a
Musab-Hassan <musabhassan04@gmail.com>
2022-08-25 14:42:42 -0700

    change public properties to private

--- src/index.ts
+++ src/index.ts
@@ -14,31 +14,31 @@ import { momentumScrollStruct } from "./struct";
 
 export default class slickScroll {
 
-    params: momentumScrollStruct;
+    #params: momentumScrollStruct;
 
-    pl: { x: number, y: number };
-    lastLocation: { x: number, y: number };
-    startStamp: number;
+    #pl: { x: number, y: number };
+    #lastLocation: { x: number, y: number };
+    #startStamp: number;
 
-    restructedElements: any;
-    rootElement: any;
-    mutationObserver: MutationObserver;
+    #restructedElements: any;
+    #rootElement: any;
+    #mutationObserver: MutationObserver;
 
-    fixedOffsets: any[] = [];
-    offsets: any[] = [];
+    #fixedOffsets: any[] = [];
+    #offsets: any[] = [];
 
-    isDestroyed: boolean;
+    #isDestroyed: boolean;
     
 
     constructor(params: momentumScrollStruct) {
         // Assign defaults to params object
-        this.params = (<any>Object).assign({}, defaults, params);
+        this.#params = (<any>Object).assign({}, defaults, params);
 
-        this.rootElement = selectNode(params.root);
+        this.#rootElement = selectNode(params.root);
 
         // Set any offsets or fixedOffsets assigned on initialization
-        this.fixedOffsets = params.fixedOffsets;
-        this.offsets = params.offsets;
+        this.#fixedOffsets = params.fixedOffsets;
+        this.#offsets = params.offsets;
 
         // Switch to mobile version of client is unsupported
         if (isCompatible()) {
@@ -52,13 +52,13 @@ export default class slickScroll {
     // Initialize momentum scrolling
     #init() {
 
-        this.restructedElements = DOMRestructure(this.rootElement);
-        let matrix = new WebKitCSSMatrix(window.getComputedStyle(this.restructedElements.fixed).transform);
-        this.lastLocation = { x: matrix.m41, y: matrix.m42 }
+        this.#restructedElements = DOMRestructure(this.#rootElement);
+        let matrix = new WebKitCSSMatrix(window.getComputedStyle(this.#restructedElements.fixed).transform);
+        this.#lastLocation = { x: matrix.m41, y: matrix.m42 }
         
         // Detect any changes to root element's appearance by listening to window resize and DOM tree changes
-        this.mutationObserver = new MutationObserver(this.#onWindowResize);
-        this.mutationObserver.observe(this.rootElement, {
+        this.#mutationObserver = new MutationObserver(this.#onWindowResize);
+        this.#mutationObserver.observe(this.#rootElement, {
             childList: true,
             attributes: true,
             subtree: true
@@ -66,7 +66,7 @@ export default class slickScroll {
         window.addEventListener("resize", this.#onWindowResize);
 
         // Bind scroll handler
-        this.rootElement.addEventListener("scroll", this.#scrollHandler);
+        this.#rootElement.addEventListener("scroll", this.#scrollHandler);
     }
 
 
@@ -74,30 +74,30 @@ export default class slickScroll {
     #scrollHandler = (e: any) => {
         
         // Get scroll position of rootElement
-        this.pl = { x: this.rootElement.scrollLeft, y: this.rootElement.scrollTop };
+        this.#pl = { x: this.#rootElement.scrollLeft, y: this.#rootElement.scrollTop };
 
-        if (typeof this.pl.x === "undefined" || typeof this.pl.y === "undefined") {
-            this.pl = { x: this.rootElement.scrollX, y: this.rootElement.scrollY, };
+        if (typeof this.#pl.x === "undefined" || typeof this.#pl.y === "undefined") {
+            this.#pl = { x: this.#rootElement.scrollX, y: this.#rootElement.scrollY, };
         }
 
         // Store time for timing animations
-        this.startStamp = Date.now();
+        this.#startStamp = Date.now();
 
         // Scroll Animation Frame Handler for easing
-        this.#animate(this.lastLocation, this.pl, this.startStamp, (position: { x: number, y: number }) => {
+        this.#animate(this.#lastLocation, this.#pl, this.#startStamp, (position: { x: number, y: number }) => {
 
             let translateString = `translate3d(${position.x}px, ${position.y}px, 0px)`;
-            this.restructedElements.fixed.style.webkitTransform = translateString;
-            this.restructedElements.fixed.style.transform = translateString;
+            this.#restructedElements.fixed.style.webkitTransform = translateString;
+            this.#restructedElements.fixed.style.transform = translateString;
 
-            this.lastLocation = position;
+            this.#lastLocation = position;
 
             // Parallax offset elements scrolling
-            if (Array.isArray(this.offsets)) {
+            if (Array.isArray(this.#offsets)) {
 
-                for (let i = 0; i < this.offsets.length; i++) {
+                for (let i = 0; i < this.#offsets.length; i++) {
 
-                    let e = Object.assign({}, defaultSpeeds, this.offsets[i]);
+                    let e = Object.assign({}, defaultSpeeds, this.#offsets[i]);
                     let offset = `translate3d(${position.x * (e.speedX - 1)}px, ${position.y * (e.speedY - 1)}px, 0)`;
                     let elements: any = selectNode(e.element, true);
 
@@ -114,11 +114,11 @@ export default class slickScroll {
             }
 
             // Set fixedOffsets position as fixed
-            if (Array.isArray(this.fixedOffsets)) {
+            if (Array.isArray(this.#fixedOffsets)) {
 
-                for (let i = 0; i < this.fixedOffsets.length; i++) {
+                for (let i = 0; i < this.#fixedOffsets.length; i++) {
                     let offset = `translate3d(${-position.x}px, ${-position.y}px, 0px)`;
-                    let node = selectNode(this.fixedOffsets[i], true);
+                    let node = selectNode(this.#fixedOffsets[i], true);
 
                     if (NodeList.prototype.isPrototypeOf(node)) {
                         for (let e of node as any) {
@@ -134,9 +134,9 @@ export default class slickScroll {
         });
 
         // Run params.onScroll function asynchrounously
-        if (this.params.onScroll) {
+        if (this.#params.onScroll) {
             new Promise(r => {
-                this.params.onScroll(e);
+                this.#params.onScroll(e);
                 r(true);
             })
         }
@@ -151,18 +151,18 @@ export default class slickScroll {
         onIterate: (position: { x: number, y: number }) => void
     ) {
         // Parse bezier easing string into number values
-        let easing = parseBezierString(this.params.easing);
+        let easing = parseBezierString(this.#params.easing);
 
         let diffX = ((tl.x * -1) - pl.x);
         let diffY = ((tl.y * -1) - pl.y);
         let x: number, y: number;
 
-        let duration = this.params.duration;
+        let duration = this.#params.duration;
 
         // Animation frame loop
         const loop = () => {
 
-            if (this.isDestroyed) return;
+            if (this.#isDestroyed) return;
 
             let t = (Date.now() - startStamp) / duration!;
 
@@ -190,11 +190,11 @@ export default class slickScroll {
     #mobileFallback() {
 
         // Parallax Offsets scrolling
-        this.rootElement.addEventListener("scroll", (event: any) => {
+        this.#rootElement.addEventListener("scroll", (event: any) => {
 
             // Offset elements scrolling
-            if (this.offsets) {
-                this.offsets.forEach((e: any) => {
+            if (this.#offsets) {
+                this.#offsets.forEach((e: any) => {
                     e = Object.assign({}, defaultSpeeds, e);
 
                     let offset = `translate3d(${event.target.scrollLeft * (1 - e.speedX)}px, ${event.target.scrollTop * (1 - e.speedY)}px, 0px)`;
@@ -216,8 +216,8 @@ export default class slickScroll {
         });
 
         // Set fixed offsets as just fixed 
-        if (this.fixedOffsets) {
-            this.fixedOffsets.forEach((element: any) => {
+        if (this.#fixedOffsets) {
+            this.#fixedOffsets.forEach((element: any) => {
                 let nodes = selectNode(element, true);
 
                 if (Symbol.iterator in Object(nodes)) {
@@ -236,8 +236,8 @@ export default class slickScroll {
 
     // Resize dummy element when window resizes to prevent overscrolling
     #onWindowResize = () => {
-        if (this.restructedElements === undefined) return;
-        this.restructedElements.dummy.style.height = this.restructedElements.fixed.scrollHeight + "px";
+        if (this.#restructedElements === undefined) return;
+        this.#restructedElements.dummy.style.height = this.#restructedElements.fixed.scrollHeight + "px";
     }
 
 
@@ -247,29 +247,29 @@ export default class slickScroll {
     // Remove all slickScroll handers and return rootelement's DOM tree to its original state
     destroy() {
         
-        let wrapper: any = this.rootElement.querySelector("._SS_wrapper");
+        let wrapper: any = this.#rootElement.querySelector("._SS_wrapper");
 
         // Remove all Observers and EventListeners
-        this.rootElement.removeEventListener("scroll", this.#scrollHandler);
-        this.rootElement.removeEventListener("resize", this.#onWindowResize);
-        this.mutationObserver.disconnect();
+        this.#rootElement.removeEventListener("scroll", this.#scrollHandler);
+        this.#rootElement.removeEventListener("resize", this.#onWindowResize);
+        this.#mutationObserver.disconnect();
 
         // Revert root element to original state and remove all slickscroll classes
         for (let i = wrapper.children.length; i > 0; i--) {
             if (wrapper.children[i - 1].removeProperty) wrapper.children[i - 1].removeProperty("transform");
-            this.rootElement.insertBefore(wrapper.children[i - 1], this.rootElement.children[0]);
+            this.#rootElement.insertBefore(wrapper.children[i - 1], this.#rootElement.children[0]);
         }
         wrapper.remove();
 
-        selectNode(this.params.root).querySelector("._SS_dummy")!.remove();
-        this.rootElement.style.removeProperty("overflow");
-        this.rootElement.style.removeProperty("position");
+        selectNode(this.#params.root).querySelector("._SS_dummy")!.remove();
+        this.#rootElement.style.removeProperty("overflow");
+        this.#rootElement.style.removeProperty("position");
 
-        this.isDestroyed = true;
+        this.#isDestroyed = true;
 
         // Clear all transformations on offsets
-        clearTransform(this.offsets);
-        clearTransform(this.fixedOffsets);
+        clearTransform(this.#offsets);
+        clearTransform(this.#fixedOffsets);
 
         // Remove "transform" from an offset array
         function clearTransform(array: any[]) {
@@ -295,10 +295,10 @@ export default class slickScroll {
 
     // Disable and remove a fixed or parallax Offset
     removeOffset(offset: string | HTMLElement | HTMLBodyElement | null) {
-        this.offsets = this.offsets.filter(item => item.element !== offset);
-        this.offsets = this.offsets.filter(item => item.element !== selectNode(offset, true));
-        this.fixedOffsets = this.fixedOffsets.filter(item => item !== offset);
-        this.fixedOffsets = this.fixedOffsets.filter(item => item !== selectNode(offset, true));
+        this.#offsets = this.#offsets.filter(item => item.element !== offset);
+        this.#offsets = this.#offsets.filter(item => item.element !== selectNode(offset, true));
+        this.#fixedOffsets = this.#fixedOffsets.filter(item => item !== offset);
+        this.#fixedOffsets = this.#fixedOffsets.filter(item => item !== selectNode(offset, true));
 
         let offsetNode = selectNode(offset, true);
 
@@ -325,24 +325,24 @@ export default class slickScroll {
         if (typeof offset !== "object") return;
 
         // Check if offset already exists
-        const find = this.offsets.find((i: any) => i.element == offset.element);
+        const find = this.#offsets.find((i: any) => i.element == offset.element);
         if (find || !offset.element) {
             return;
         }
 
         // Assign default values to offset and push it to offsets array
         offset = Object.assign({}, defaultSpeeds, offset);
-        this.offsets.push(offset);
+        this.#offsets.push(offset);
     }
 
 
     addFixedOffset(element: string | HTMLElement | HTMLBodyElement | null) {
 
         // Check if offset already exists
-        if (!selectNode(element, true) || this.fixedOffsets.includes(element)) {
+        if (!selectNode(element, true) || this.#fixedOffsets.includes(element)) {
             return;
         }
 
-        this.fixedOffsets.push(element);
+        this.#fixedOffsets.push(element);
     }
 }
